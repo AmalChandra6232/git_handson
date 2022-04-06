5f4c43bc8d84e4b3854fb76857fb71e4c1dff50a
Your Name <you@example.com>
2022-04-06 14:50:56 +0530

    Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Reverse
+{
+ static int reverseNumber(int num)
+ {
+	 int rev=0;
+	 int remainder;
+	 while(num>0)
+	 {
+		 remainder=num%10;
+		 rev=(rev*10) + remainder;
+		 num=num/10;
+	 }
+ 
+		return rev;
+	 }
+  
+ public static void main(String[] args)
+ {
+	 int num;
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("Enter the number");
+	 num=sc.nextInt();
+ System.out.println("Reversed Number is "+ reverseNumber(num));
+ }
+}
